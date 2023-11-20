9f9a29509ed55fc7e839488939858115f138b605
siddhantgarg_umass_edu <siddhantgarg_umass_edu@gypsum-gpu116.unity.rc.umass.edu>
2023-11-20 16:49:28 +0000

    generate summaries code

--- eval.py
+++ eval.py
@@ -1,59 +1,97 @@
 from datasets import load_dataset 
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, GenerationConfig, TrainingArguments, Trainer 
+from datasets import concatenate_datasets
+
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+from transformers import DataCollatorForSeq2Seq
+from transformers import Seq2SeqTrainer, Seq2SeqTrainingArguments
+
 import torch
-import time 
-import evaluate 
 import pandas as pd 
 import numpy as np 
-
-from utils import *
+from functools import partial
+import os, sys
+from tqdm import tqdm 
 import evaluate
 
+import argparse
+from utils import *
+from peft import LoraConfig, get_peft_model, TaskType
+
 device = torch.device("cuda:0" if torch.cuda.is_available() else torch.device('cpu'))
+print('device:', device)
 
-hf_data_name = 'knkarthick/dialogsum'
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--dataset_name', type=str, default='samsum')
+parser.add_argument('--model_name', type=str, default='google/flan-t5-base')
+parser.add_argument('--checkpoint_dir', type=str, default='trained_models')
+parser.add_argument('--enable_lora', action='store_true')
+
+args = parser.parse_args() 
+
+hf_data_name = args.dataset_name
 dataset = load_dataset(hf_data_name, cache_dir='./data')
+print(f'HF dataset--{hf_data_name} loaded')
 
-# print(dataset)
+tokenizer = AutoTokenizer.from_pretrained(args.model_name, cache_dir='./models')
 
-saved_model_dir = f'./dialogue-summary-training'
+model_name = args.model_name
+model = AutoModelForSeq2SeqLM.from_pretrained(args.checkpoint_dir, torch_dtype=torch.bfloat16)
+if args.enable_lora:
+    lora_config = LoraConfig(
+        r=32, # Rank
+        lora_alpha=32,
+        target_modules=["q", "v"],
+        lora_dropout=0.05,
+        bias="none",
+        task_type=TaskType.SEQ_2_SEQ_LM # FLAN-T5
+    )
+    model = get_peft_model(model, lora_config)
 
-model_name = 'google/flan-t5-base'
+print(f'HF {model_name} loaded')
+model.eval()
 
-model = AutoModelForSeq2SeqLM.from_pretrained(saved_model_dir, torch_dtype=torch.bfloat16)
-tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir='./models')
+sample = dataset['test'][200]
+dialogue = sample['dialogue']
+human_summary = sample['summary']
 
-dialogues = dataset['test'][0:10]['dialogue']
-human_baseline_summaries = dataset['test'][0:10]['summary']
+prompt = """Summarize the conversation between triple backticks \n
+```
+{}
+```\n
+Summary: """
 
-instruct_model_summaries = []
+human_generated = []
+model_generated = []
 
-for _, dialogue in enumerate(dialogues):
-    prompt = f"""
-        Summarize the following conversation.
+for sample in tqdm(dataset['test']):
+    dialogue = sample['dialogue']
+    human_summary = sample['summary']
 
-        {dialogue}
+    inputs = tokenizer(prompt.format(dialogue), return_tensors='pt')
+    model_summary = tokenizer.decode(
+        model.generate(
+            input_ids=inputs['input_ids'],
+            max_new_tokens=200,
+        )[0],
+        skip_special_tokens=True
+    )
 
-        Summary: """
-    input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+    human_generated.append(human_summary)
+    model_generated.append(model_summary)
+    # break
 
-    instruct_model_outputs = model.generate(input_ids=input_ids, generation_config=GenerationConfig(max_new_tokens=200))
-    instruct_model_text_output = tokenizer.decode(instruct_model_outputs[0], skip_special_tokens=True)
-    instruct_model_summaries.append(instruct_model_text_output)
-    
-zipped_summaries = list(zip(human_baseline_summaries, instruct_model_summaries))
- 
-df = pd.DataFrame(zipped_summaries, columns = ['human_baseline_summaries', 'instruct_model_summaries'])
-print(df.head)
+outputs = {
+    'Human Summaries' : human_generated,
+    'Model Generated Summaries' : model_generated,
+}
+df = pd.DataFrame(outputs)
 
-rouge = evaluate.load('rouge')
+output_csv_path = os.path.join(args.checkpoint_dir, 'generated_summaries.csv')
+df.to_csv(output_csv_path)
 
-instruct_model_results = rouge.compute(
-    predictions=instruct_model_summaries,
-    references=human_baseline_summaries[0:len(instruct_model_summaries)],
-    use_aggregator=True,
-    use_stemmer=True,
-)
-
-print('INSTRUCT MODEL:')
-print(instruct_model_results)
+'''
+347
+356
+357
+'''
